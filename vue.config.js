a658da5e154a4483d40d8ae4adcb78a00575b187
tfx2001 <2479727366@qq.com>
2020-02-02 23:02:39 +0800

    :heavy_plus_sign: Add Vuetify.js and G2Plot

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chainWebpack: config => {
+    config.plugin('VuetifyLoaderPlugin').tap(args => [{
+      match (originalTag, { kebabTag, camelTag, path, component }) {
+        if (kebabTag.startsWith('core-')) {
+          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+        }
+      }
+    }])
+  }
+}
